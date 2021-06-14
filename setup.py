918bbd0404f6541eafc8e77eb6e0ad37e153528d
Teosof <liskunov.roma@yandex.ru>
2021-06-14 17:59:41 +0300

    [no task] adding initialization

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='flaskProject',
+    version='1.0.0',
+    packages=[''],
+    url='localhost:5000/tutorials',
+    license='MIT',
+    author='rgliskunov',
+    author_email='liskunov.roma@yandex.ru',
+    description='Test REST API application'
+)
